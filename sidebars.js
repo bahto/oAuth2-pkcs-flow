8c9f28a83ad731172d608984fa775cbd27c94c77
Bahto <bahto@the-b-it.nl>
2023-02-08 20:59:13 +0100

    Init files

---
+++ sidebars.js
@@ -0,0 +1,14 @@
+// external_script.js
+
+function displayMessage() {
+    alert("Hello from external script!");
+  }
+  
+  /* global bootstrap: false */
+(() => {
+  'use strict'
+  const tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  tooltipTriggerList.forEach(tooltipTriggerEl => {
+    new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+})()
